1b9e2d1067a7ab8aaa0f42a0f527b7b65343dbf4
樋口 凱一 <36272933+yoshi0319@users.noreply.github.com>
2025-03-21 16:21:02 +0900

    Revert "DBの修正"

--- backend/money_management/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, MoneyViewSet, RecordViewSet
-
-router = DefaultRouter()
-router.register("user", UserViewSet)
-router.register("money", MoneyViewSet)
-router.register("record", RecordViewSet)
-
-urlpatterns = router.urls
--- backend/my_project/urls.py
+++ backend/my_project/urls.py
@@ -14,22 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from money_management.views import (
-    UserViewSet,
-    MoneyViewSet,
-    RecordViewSet,
-)
-
-router = DefaultRouter()
-router.register(r"user", UserViewSet)
-router.register(r"money", MoneyViewSet)
-router.register(r"record", RecordViewSet)
+from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include(router.urls)),
+    path('admin/', admin.site.urls),
 ]
