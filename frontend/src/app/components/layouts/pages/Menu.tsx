47a0c384880c106f9086a57765fc7e6bfe019b03
yoshi0319 <yoshi3YH@icloud.com>
2025-05-20 14:05:36 +0900

    ログアウトの実装

--- frontend/src/app/components/layouts/pages/Menu.tsx
+++ frontend/src/app/components/layouts/pages/Menu.tsx
@@ -1,13 +1,24 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { TabContext } from "@/app/components/layouts/Header/Header"
 import Image from "next/image"
+import { useRouter } from "next/navigation";
 
 export default function Menu() {
+  const router = useRouter();
+  
   const { tabIndex, setTabIndex, user } = useContext(TabContext)
+  const [settingsOpen, setSettingsOpen] = useState(false);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("rememberMe");
+    router.push("/auth/login");
+  }
+
   return (
     <div className="w-full h-16 bg-white border-b border-[#E2E8F0]">
         <div className="flex flex-row justify-between items-center w-full h-full px-4">
-            
+
             <div className="w-1/4"></div>
             
             <div className="flex flex-row justify-center items-center w-1/2">
@@ -34,7 +45,7 @@ export default function Menu() {
             </div>
 
             <div className="w-1/4 flex justify-end items-center gap-2 pr-8">
-                <div className="flex justify-end items-center gap-2 hover:bg-gray-100 rounded-md p-2">
+                <button onClick={() => setSettingsOpen(!settingsOpen)} className={`flex justify-end items-center gap-2 border-2 hover:bg-gray-100 rounded-md p-2 ${settingsOpen ? "border-2 bg-gray-100 border-gray-200": "border-transparent"}`}>
                     <label className="flex flex-col cursor-pointer">
                         <span className="text-sm font-medium">{user?.user_name || "ゲスト"}</span>
                         <span className="text-xs text-gray-500">{user?.email_address || ""}</span>
@@ -47,9 +58,14 @@ export default function Menu() {
                             height={20}
                             />
                     </div>
-                </div>
+                </button>
+                {settingsOpen && (
+                    <div className="absolute top-15 right-12 w-30 bg-white border-1 border-gray-200 shadow-md px-2 py-1">
+                        <button onClick={handleLogout} className="text-sm text-red-500 hover:bg-gray-100">ログアウト</button>
+                    </div>
+                )}
             </div>
         </div>
     </div>
-  )
+    )
 }
