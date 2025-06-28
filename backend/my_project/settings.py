9dbac89b9daab8c40bc5a246503ce5508551870b
yoshi0319 <yoshi3YH@icloud.com>
2025-06-29 00:29:22 +0900

    backendのバグ修正

--- backend/my_project/settings.py
+++ backend/my_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS", "").split(" ")]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
 
 
 # Application definition
